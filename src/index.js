a9727c16570f8281e980281eeb3b810055456ef2
UltraDEV007 <LeTuan2024@outlook.com>
2016-11-11 14:31:35 -0800

    pass down props from Accounts object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import Accounts from './Accounts';
+import { withProps } from 'recompose';
+import Accounts from '@accounts/accounts';
 import accountRoutes from './accountRoutes';
 
-export default Accounts;
-
 export { accountRoutes };
+
+const connectAccounts = Component =>
+    withProps(ownProps => ({
+      ...ownProps,
+      config: Accounts.ui._options,
+      formType: Accounts.getState().formType,
+      loginForm: Accounts.getState().forms.loginForm,
+      signupForm: Accounts.getState().forms.signupForm,
+    }))(Component);
+
+
+export { connectAccounts };
