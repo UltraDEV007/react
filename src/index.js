e2fa7dcef2c477699a6e479b7b7665a2a7c64d5a
UltraDEV007 <LeTuan2024@outlook.com>
2016-11-12 20:59:15 -0800

    connect component to redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import { withProps } from 'recompose';
+import { connect } from 'react-redux';
 import Accounts from '@accounts/accounts';
 import accountRoutes from './accountRoutes';
 
 export { accountRoutes };
 
-const connectAccounts = Component =>
-    withProps(ownProps => ({
-      ...ownProps,
+const connectAccounts = (Component) => {
+  const mapStateToProps = (state) => {
+    // In case the consuming appplication has an immutable redux store
+    const accounts = Accounts.getAccountsState(state);
+    return ({
+      formType: accounts.formType,
+      login: accounts.forms.login,
       config: Accounts.ui._options,
-      formType: Accounts.getState().formType,
-      loginForm: Accounts.getState().forms.loginForm,
-      signupForm: Accounts.getState().forms.signupForm,
-    }))(Component);
+    });
+  };
 
+  const mapDispatchToProps = () => ({
+    onSubmitLogin: ({ user, password }) => Accounts.login({ user, password }),
+    onChangeField: ({ form, field, value }) => Accounts.setField({ form, field, value }),
+  });
+
+  return connect(mapStateToProps, mapDispatchToProps)(Component);
+};
 
 export { connectAccounts };
