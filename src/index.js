9930b0b18ee229f3c056f0d3a8f60cd2a98963eb
UltraDEV007 <LeTuan2024@outlook.com>
2016-11-13 23:41:34 -0800

    renderFormComponent and proptypes for Accounts component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Accounts from '@accounts/accounts';
 import accountRoutes from './accountRoutes';
 
 export { accountRoutes };
+
+const accountsPropTypes = {
+  formType: PropTypes.string.isRequired,
+  config: PropTypes.object.isRequired,
+  login: PropTypes.object.isRequired,
+  onSubmitLogin: PropTypes.func.isRequired,
+  onChangeField: PropTypes.func.isRequired,
+  user: PropTypes.object,
+};
+
+export { accountsPropTypes };
+
+const renderFormComponent = (formType, props) => {
+  let form;
+  switch (formType) {
+    case 'login':
+      form = <Accounts.ui.LoginForm {...props} />;
+      break;
+    default:
+      break;
+  }
+  return form;
+};
+
+export { renderFormComponent };
 
 const connectAccounts = (Component) => {
   const mapStateToProps = (state) => {
@@ -11,6 +37,7 @@ const connectAccounts = (Component) => {
     return ({
       formType: accounts.formType,
       login: accounts.forms.login,
+      user: accounts.user,
       config: Accounts.ui._options,
     });
   };
