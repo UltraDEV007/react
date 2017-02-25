53bb8174d241303f228bbc9ad01e18a7619c7cb4
UltraDEV007 <LeTuan2024@outlook.com>
2017-02-25 19:04:35 +0200

    * Moved accounts/client and accounts/common to peer dependency * Removed config method

--- packages/react-native/src/index.js
+++ packages/react-native/src/index.js
@@ -1,18 +1,3 @@
 // @flow
-import { AsyncStorage } from 'react-native';
-import AccountsClient, { reducer } from '@accounts/client';
-import type { TransportInterface } from '@accounts/client';
-import type { Store } from 'redux';
-
 export * from './login';
 export * from './reducer';
-
-export const config = (transport: TransportInterface, store?: Store<Object>) => AccountsClient.config({
-  store,
-  tokenStorage: AsyncStorage,
-  onEnrollAccountHook: () => null,
-  onResetPasswordHook: () => null,
-  onVerifyEmailHook: () => null,
-  onSignedInHook: () => null,
-  onSignedOutHook: () => null,
-}, transport);
